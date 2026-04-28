02eb4a57261abc1d1a845d2b79d81319a55f2281
Prashanth Chowdary <prashanth.chowdary@webileapps.com>
2026-04-28 16:10:32 +0530

    adding items to popup menu

--- extension.js
+++ extension.js
@@ -4,6 +4,7 @@ import St from 'gi://St';
 import GObject from 'gi://GObject';
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
+import * as PopupMenu from 'resource:///org/gnome/shell/ui/popupMenu.js';
 
 let indicator = null;
 
@@ -18,13 +19,16 @@ class MyIndicator extends PanelMenu.Button {
         });
 
         this.add_child(icon);
+				
+				this.menu.addMenuItem(new PopupMenu.PopupMenuItem('test'));
+				this.menu.addMenuItem(new PopupMenu.PopupMenuItem('test2'));
     }
 });
 
 export default class Extension {
     enable() {
         indicator = new MyIndicator();
-        Main.panel.addToStatusArea('cpu-monitor', indicator, 0, 'right');
+        Main.panel.addToStatusArea('cpu-monitor', indicator, 1, 'right');
     }
 
     disable() {
